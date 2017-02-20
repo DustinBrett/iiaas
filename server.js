cfb1c4767440e207cab5cc0039a8741ae0013784
Dustin Brett <dustinbrett@gmail.com>
2017-02-20 15:58:26 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,96 +3,105 @@ const fs = require("fs");
 class database {
   constructor(path) {
     this.path = path;
-    this.users = require(this.path);
+    this.users = require(this.path); // Load json database file
   }
 
-  save() {
+  save() { // Write changes to json database file
     fs.writeFile(this.path, JSON.stringify(this.users, null, 2), "utf8");
   }
 }
 
 const
-  dbPath = __dirname + "/users.json",
-  reqPath = "/v1/:endpoint",
-
   express = require("express")(),
   bodyParser = require("body-parser"),
   bearerToken = require("express-bearer-token"),
   hat = require("hat"),
 
-  db = new database(dbPath),
+  db = new database(__dirname + "/users.json"),
 
-  createUser = (email, password) => {
-    const token = hat();
-
-    db.users[token] = {
-      "email": email,
-      "password": password,
-      "integer": 1
-    };
-
-    db.save();
-
-    return token;
-  },
-
-  setInteger = (token, integer) => {
-    if (typeof integer === "number") {
-      db.users[token].integer = integer;
-    } else {
-      db.users[token].integer++;
+  findToken = (email, password) => { // Search database for existing credentials
+    for (let token in db.users) {
+      if (db.users[token].email === email && db.users[token].password === password) {
+        return token; // Return API key if credentials exist
+      }
     }
-
-    db.save();
   },
 
   confirmAuth = (token) => {
     return (typeof db.users[token] === "object");
   },
 
+  createUser = (email, password) => { // Create new user with api key (if required)
+    const token = findToken(email, password) || hat();
+
+    if (!confirmAuth(token)) { // Create user structure if non-existent
+      db.users[token] = {
+        "email": email,
+        "password": password,
+        "integer": 1
+      };
+
+      db.save();
+    }
+
+    return token;
+  },
+
+  setInteger = (token, integer) => {
+    db.users[token].integer = (
+      integer
+      ? integer
+      : db.users[token].integer + 1
+    );
+
+    db.save();
+  },
+
   checkEndPoint = (req) => {
     if (req.params.endpoint === "register") {
-      if (req.body.email && req.body.password) {
-        return createUser(req.body.email, req.body.password);
-      }
-
-      return "An email address and a password are required for registration.";
+      return (
+        (req.body.email && req.body.password)
+        ? createUser(req.body.email, req.body.password)
+        : "An email address and password are required for registration."
+      );
     }
 
     if (req.token && confirmAuth(req.token)) {
-      if (req.params.endpoint === "next") {
+      if (req.params.endpoint === "next") { // Increment integer by 1
         setInteger(req.token);
-      } else if (req.body.current) {
+      } else if (req.body.current) { // Set integer to specified value
         const newInt = parseInt(req.body.current);
 
-        if (newInt < 0) {
+        if (newInt < 0) { // Disallow non-negative integers
           return "Integer must be a non-negative value.";
         }
 
         setInteger(req.token, newInt);
       }
 
-      return db.users[req.token].integer.toString();
+      return db.users[req.token].integer.toString(); // Respond with current integer
     }
 
     return "REST endpoint secured. API key is required.";
   },
 
   reqHandler = (req, res) => {
-    if (req.params.endpoint) {
-      res.send(checkEndPoint(req));
-    } else {
-      res.send("REST endpoint required.");
-    }
+    res.send(
+      req.params.endpoint
+      ? checkEndPoint(req)
+      : "REST endpoint required."
+    );
   };
 
 express.use(bearerToken());
 express.use(bodyParser.urlencoded({ extended: false }));
 
-express.get(reqPath, reqHandler);
-express.put(reqPath, reqHandler);
-express.post(reqPath, reqHandler);
+express.route("/v1/:endpoint") // Monitor GET/PUT/POST requests
+  .get(reqHandler)
+  .put(reqHandler)
+  .post(reqHandler)
+;
 
-express.listen(3000, function() {
-  console.log("Listening @ http://localhost:3000/v1/ ...");
+express.listen(3000, () => {
+  console.log("Listening on port 3000.");
 });
